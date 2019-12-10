db2a472a6a9bcb550138f3e9adda6637ad955a6e
Emimake <emysteven@gmail.com>
2019-12-10 12:40:51 +0100

    DEBUG Edited

--- cedar/cedar/settings.py
+++ cedar/cedar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-$4bor-2=pw4&xsdh*8xqnz+e4$5@8lcm!=(n@2-f61*p-5vla'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.mindpatrons.com']
 
